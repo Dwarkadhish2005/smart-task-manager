751b11a86bc734f127721b9ad8958b36bf91d7d6
Dwarkadhish Kamthane <72198604+Dwarkadhish2005@users.noreply.github.com>
2026-06-02 22:34:28 +0530

    initial project start

---
+++ src/main/java/com/smarttask/App.java
@@ -0,0 +1,16 @@
+package com.smarttask;
+
+public class App {
+public static void main(String[] args) {
+
+    System.out.println("Smart Task Manager Running Successfully!");
+
+}
+
+public int addTasks(int a, int b) {
+
+    return a + b;
+
+}
+
+}
---
+++ src/test/java/com/smarttask/AppTest.java
@@ -0,0 +1,10 @@
+package com.smarttask;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+@Test
+public void testAddTasks() {
+    App app = new App();
+    assertEquals(10, app.addTasks(5,5));
+}
+}
